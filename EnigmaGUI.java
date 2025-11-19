de827819db9b1fd4ea436b203a940ca1b4445897
kushaan1 <kushaan.nagpal02@gmail.com>
2025-11-19 01:33:06 -0500

    add logic for GUI class to respond to interactions (controller)

--- EnigmaGUI.java
+++ EnigmaGUI.java
@@ -1,7 +1,25 @@
 public class EnigmaGUI {
 
     public static void main(String[] args) {
+        EnigmaFrame frame = new EnigmaFrame();
+        frame.encryptBtn.addActionListener(e -> processChoices(frame, true));
+        frame.decryptBtn.addActionListener(e -> processChoices(frame, false));
+    }
+    public static void processChoices(EnigmaFrame frame, boolean isEncrypt){
+        int inner = frame.innerBox.getSelectedItem(); 
+        int middle = frame.middleBox.getSelectedItem();
+        int outer = frame.outerBox.getSelectedItem();
+        String startPos = frame.startingPos.getText();    
+        String input = frame.inputText.getText();
+        Enigma enigma = new Enigma(inner, middle, outer, startPos);
 
+        if(isEncrypt){
+            String encryptedText = enigma.encrypt(input);
+            frame.outputText.setText(encryptedText);
+        } else {
+            String decryptedText = enigma.decrypt(input);
+            frame.outputText.setText(decryptedText);
+        }
     }
 
 }
