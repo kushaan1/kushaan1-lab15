2f04c847fcf89e3b030fb60da520745abb0c9f9f
kushaan1 <kushaan.nagpal02@gmail.com>
2025-11-18 21:00:52 -0500

    add enigmaframe enigmaGUI

---
+++ EnigmaFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class EnigmaFrame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(300, 400);
+        frame.setTitle("Enigma GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+
+}
